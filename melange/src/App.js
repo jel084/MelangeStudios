bd64e10825aa9ae00f7b69a7e3236ecae41c32fa
Jeanine Le <jeaninele9624@gmail.com>
2025-05-13 21:45:02 -0700

    skeleton added

--- melange/src/App.js
+++ melange/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>the components will be listed here!!!</h1>
+      <h1> NavBar </h1>
+      <h1> Intro </h1>
+      <h1> About </h1>
+      <h1> Shabnam </h1>
+      <h1> Contact </h1>
     </div>
   );
 }
---
+++ melange/src/components/navbar/navbar.js
@@ -0,0 +1,17 @@
+// import React from "react";
+// import'./navbar.css'
+// import {Link} from 'react-scroll'
+
+// const Navbar = () => {
+//     return (
+//         <nav className="navbar">
+//             <div className="desktopMenu">
+//                 <Link activeClass="active" to='home' spy={true} smooth={true} offset={-70} duration={500} className="desktopMenuListItem">Home</Link>
+//                 <Link activeClass="active" to='aboutme' spy={true} smooth={true} offset={-40} duration={500} className="desktopMenuListItem">About</Link>
+//                 <Link activeClass="active" to='projects' spy={true} smooth={true} offset={-40} duration={500} className="desktopMenuListItem">Portfolio</Link>
+//             </div>
+//         </nav>
+//     )
+// }
+
+// export default Navbar
